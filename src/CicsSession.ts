1269619019bdd9fc0efa7cfbb0ec4cdd3af6aa3d
AndrewTwydell <u1858490@unimail.hud.ac.uk>
2021-04-12 16:33:09 +0100

    using extender apis
    Signed-off-by: AndrewTwydell <u1858490@unimail.hud.ac.uk>

--- src/CicsSession.ts
+++ src/CicsSession.ts
@@ -2,8 +2,8 @@ import * as imperative_1 from '@zowe/imperative';
 
 export class CicsSession {
 
-    session: imperative_1.Session
-    context: string
+    session: imperative_1.Session;
+    context: string;
 
     constructor(session: imperative_1.Session, context: string) {
         this.session = session;
--- src/extension.ts
+++ src/extension.ts
@@ -1,213 +1,317 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-import * as cics from '@zowe/cics-for-zowe-cli';
-import * as imperative_1 from '@zowe/imperative';
-import { CicsSession } from './CicsSession';
+import * as vscode from "vscode";
+import * as cics from "@zowe/cics-for-zowe-cli";
+import * as imperative_1 from "@zowe/imperative";
+import { CicsSession } from "./CicsSession";
+import { CicsApi } from "./CicsApi";
+
+import { ZoweExplorerApi, ProfilesCache } from "@zowe/zowe-explorer-api";
+import { Logger } from "@zowe/imperative";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  void registerCICSApi();
 
-	// CICS View
-	const treeDataProv = new TreeDataProvider();
-	vscode.window.registerTreeDataProvider('cics-view', treeDataProv);
+  // CICS View
+  const treeDataProv = new TreeDataProvider();
+  vscode.window.registerTreeDataProvider("cics-view", treeDataProv);
+
+  const addSession = vscode.commands.registerCommand(
+    "cics-extension-for-zowe.addSession",
+    async () => treeDataProv.addSession()
+  );
+  context.subscriptions.push(addSession);
+  treeDataProv.refresh();
+
+  let newCopyCommand = vscode.commands.registerCommand(
+    "cics-extension-for-zowe.newCopyProgram",
+    (node) => {
+      // The code you place here will be executed every time your command is executed
+      if (node) {
+        const response = cics.programNewcopy(node.session, {
+          name: node.label,
+          regionName: "IY3BNCAF",
+          cicsPlex: undefined,
+        });
+        vscode.window.showInformationMessage(
+          `New Copy Requested for program ${node.label}`
+        );
+      } else {
+        vscode.window.showErrorMessage("No CICS program selected");
+      }
+    }
+  );
+
+  context.subscriptions.push(newCopyCommand);
+}
+
+async function registerCICSApi() {
+  const zoweExplorerApi = vscode.extensions.getExtension(
+    "Zowe.vscode-extension-for-zowe"
+  );
+
+  if (zoweExplorerApi && zoweExplorerApi.exports) {
+    const importedApi = zoweExplorerApi.exports as ZoweExplorerApi.IApiRegisterClient;
+
+    Logger.initLogger({
+      log4jsConfig: {
+        appenders: {
+          default: {
+            type: "fileSync",
+            layout: {
+              type: "pattern",
+              pattern: "[%d{yyyy/MM/dd} %d{hh:mm:ss.SSS}] [%p] %m",
+            },
+            filename: "logs/imperative.log",
+          },
+          imperative: {
+            type: "fileSync",
+            layout: {
+              type: "pattern",
+              pattern: "[%d{yyyy/MM/dd} %d{hh:mm:ss.SSS}] [%p] %m",
+            },
+            filename: "logs/imperative.log",
+          },
+          app: {
+            type: "fileSync",
+            layout: {
+              type: "pattern",
+              pattern: "[%d{yyyy/MM/dd} %d{hh:mm:ss.SSS}] [%p] %m",
+            },
+            filename: "logs/zowe.log",
+          },
+        },
+        categories: {
+          default: {
+            appenders: ["default"],
+            level: "DEBUG",
+          },
+          imperative: {
+            appenders: ["imperative"],
+            level: "DEBUG",
+          },
+          app: {
+            appenders: ["app"],
+            level: "DEBUG",
+          },
+        },
+      },
+    });
+    let pp = new ProfilesCache(Logger.getAppLogger());
+
+    console.log(pp);
+    console.log(pp.getDefaultProfile());
 	
-	const addSession = vscode.commands.registerCommand("cics-extension-for-zowe.addSession", async () => treeDataProv.addSession());
-	context.subscriptions.push(addSession);
-	treeDataProv.refresh();
 
-	let newCopyCommand = vscode.commands.registerCommand('cics-extension-for-zowe.newCopyProgram', (node) => {
-		// The code you place here will be executed every time your command is executed
-		if (node) {
-			const response = cics.programNewcopy(node.session, {
-				name: node.label,
-				regionName: "IY3BNCAF",
-				cicsPlex: undefined
-			});
-			vscode.window.showInformationMessage(`New Copy Requested for program ${node.label}`);
-		} else {
-			vscode.window.showErrorMessage("No CICS program selected");
-		}
-	});
+    // let n = new CicsApi();
+    // console.log(n.getSession());
 
-	context.subscriptions.push(newCopyCommand);
+    if (
+      importedApi.getExplorerExtenderApi &&
+      importedApi.getExplorerExtenderApi().reloadProfiles
+    ) {
+      await importedApi.getExplorerExtenderApi().reloadProfiles();
+    }
+    void vscode.window.showInformationMessage(
+      "Zowe Explorer was modified for the CICS Extension"
+    );
+    return true;
+  }
+  void vscode.window.showInformationMessage(
+    "Zowe Explorer was not found: either it is not installed or you are using an older version without extensibility API."
+  );
+  return false;
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {}
 
 class TreeDataProvider implements vscode.TreeDataProvider<TreeItem> {
-	// onDidChangeTreeData?: vscode.Event<TreeItem | null | undefined> | undefined;
-	private _onDidChangeTreeData: vscode.EventEmitter<any | undefined> = new vscode.EventEmitter<any | undefined>();
-	readonly onDidChangeTreeData: vscode.Event<any | undefined> = this._onDidChangeTreeData.event;
+  // onDidChangeTreeData?: vscode.Event<TreeItem | null | undefined> | undefined;
+  private _onDidChangeTreeData: vscode.EventEmitter<
+    any | undefined
+  > = new vscode.EventEmitter<any | undefined>();
+  readonly onDidChangeTreeData: vscode.Event<any | undefined> = this
+    ._onDidChangeTreeData.event;
 
-	private sessionMap = new Map/*<string, imperative_1.Session>*/(); 
-	data: TreeItem[];
+  private sessionMap = new Map /*<string, imperative_1.Session>*/();
+  private data: TreeItem[] = [];
 
-	cicsData = {
-		profiles: []
-	};
+  async refresh(): Promise<void> {
+    try {
+      let profileList = [];
+      if (this.sessionMap.size === 0) {
+        profileList.push({
+          name: "Create a new CICS Session...",
+          session: undefined,
+          contexts: [],
+          programs: [],
+        });
+      }
+      for (const element of this.sessionMap) {
+        const session = element[1].session;
+        const context = element[1].context;
+        const name = element[0];
 
-	async refresh(): Promise<void> {
-		try {
-			let profileList = [];
-			if (this.sessionMap.size == 0) {
-				profileList.push({
-					'name': 'Create a new CICS Session...',
-					'Contexts': [],
-					'Programs': []
-				});
-			}
-			for (const element of this.sessionMap) {
-				const session = element[1].session;
-				const context = element[1].context;
-				const name = element[0];
+        const regionResponse = await cics.getResource(session, {
+          name: "CICSRegion",
+          regionName: context,
+          cicsPlex: undefined,
+          criteria: undefined,
+          parameter: undefined,
+        });
+        let resRegions: any = regionResponse.response.records["cicsregion"];
+        if (!Array.isArray(resRegions)) {
+          resRegions = [resRegions];
+        }
+        const contexts = resRegions.map((region: any) => region.applid);
 
-				let current_profile = {};
-				current_profile['name'] = name;
-				current_profile['session'] = session;
+        const programResponse = await cics.getResource(session, {
+          name: "CICSProgram",
+          regionName: context,
+          cicsPlex: undefined,
+          criteria: undefined,
+          parameter: undefined,
+        });
+        let resPrograms: any[] =
+          programResponse.response.records["cicsprogram"];
+        if (!Array.isArray(resPrograms)) {
+          resPrograms = [resPrograms];
+        }
+        const programs = resPrograms.map((program) => program.program);
 
-				const regionResponse = await cics.getResource(session, {
-					name: "CICSRegion",
-					regionName: context,
-					cicsPlex: undefined,
-					criteria: undefined,
-					parameter: undefined
-				});
-				let resRegions: any = regionResponse.response.records["cicsregion"];
-				if (!Array.isArray(resRegions)) {
-					resRegions = [resRegions]
-				}
-				const contexts = resRegions.map(region => region.applid);
-				current_profile['Contexts'] = contexts;
+        let currentProfile = {
+          name: name,
+          session: session,
+          contexts: contexts,
+          programs: programs,
+        };
 
-				const programResponse = await cics.getResource(session, {
-					name: "CICSProgram",
-					regionName: context,
-					cicsPlex: undefined,
-					criteria: undefined,
-					parameter: undefined
-				});
-				let resPrograms: any[] = programResponse.response.records["cicsprogram"];
-				if (!Array.isArray(resPrograms)) {
-					resPrograms = [resPrograms]
-				}
-				const programs = resPrograms.map(program => program.program);
-				current_profile['Programs'] = programs;
-				profileList.push(current_profile);
-			}
+        profileList.push(currentProfile);
+      }
 
-			this.cicsData = {
-				profiles: profileList
-			};
-			
-			this.data = this.cicsData.profiles.map(profile => {
-				const session = profile['session'];
-				const contexts = profile['Contexts'].map(applid => new TreeItem(applid, session));
-				const programs = profile['Programs'].map(progName => {
-					const programItem = new TreeItem(progName, session);
-					programItem.contextValue = `cicsprogram.${progName}`;
-					return programItem;
-				});
-				let profileChildren = []
-				if (contexts.length > 0) {
-					profileChildren.push(new TreeItem('Contexts', session, contexts))
-				}
-				if (programs.length > 0) {
-					profileChildren.push(new TreeItem('Programs', session, programs))
-				}
-				const profileTree = new TreeItem(profile.name, session, profileChildren);
-				return profileTree;
-			});
-			this._onDidChangeTreeData.fire(undefined);
-		} catch (error) {
-			console.log(error);
-		}
-	};
+      this.data = profileList.map((profile) => {
+        const session = profile.session;
+        const contexts = profile.contexts.map(
+          (applid: string) => new TreeItem(applid, session)
+        );
+        const programs = profile.programs.map((progName) => {
+          const programItem = new TreeItem(progName, session);
+          programItem.contextValue = `cicsprogram.${progName}`;
+          return programItem;
+        });
+        let profileChildren = [];
+        if (contexts.length > 0) {
+          profileChildren.push(new TreeItem("Contexts", session, contexts));
+        }
+        if (programs.length > 0) {
+          profileChildren.push(new TreeItem("Programs", session, programs));
+        }
+        const profileTree = new TreeItem(
+          profile.name,
+          session,
+          profileChildren
+        );
+        return profileTree;
+      });
+      this._onDidChangeTreeData.fire(undefined);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-	public async addSession() {
-		//Prompt for details
-		let options = {
-			placeHolder: "Session Name",
-			prompt: "Enter a name for the connection",
-			value: ""
-		};
-		const sessionName = await vscode.window.showInputBox(options);
+  public async addSession() {
+    //Prompt for details
+    let options = {
+      placeHolder: "Session Name",
+      prompt: "Enter a name for the connection",
+      value: "",
+    };
+    const sessionName = await vscode.window.showInputBox(options);
 
-		options = {
-			placeHolder: "Host name",
-			prompt: "Enter the host name for the connection",
-			value: ""
-		};
-		const hostname = await vscode.window.showInputBox(options);
+    options = {
+      placeHolder: "Host name",
+      prompt: "Enter the host name for the connection",
+      value: "",
+    };
+    const hostname = await vscode.window.showInputBox(options);
 
-		options = {
-			placeHolder: "Port",
-			prompt: "Enter the port for the connection",
-			value: "0"
-		};
-		const port = await vscode.window.showInputBox(options);
+    options = {
+      placeHolder: "Port",
+      prompt: "Enter the port for the connection",
+      value: "0",
+    };
+    const port = await vscode.window.showInputBox(options);
 
-		options = {
-			placeHolder: "Username",
-			prompt: "Enter the user name used for the connection",
-			value: ""
-		};
-		const username = await vscode.window.showInputBox(options);
+    options = {
+      placeHolder: "Username",
+      prompt: "Enter the user name used for the connection",
+      value: "",
+    };
+    const username = await vscode.window.showInputBox(options);
 
-		let passOptions = {
-			placeHolder: "Password",
-			prompt: "Enter the password used for the connection",
-			value: "",
-			password: true
-		};
-		const password = await vscode.window.showInputBox(passOptions);
+    let passOptions = {
+      placeHolder: "Password",
+      prompt: "Enter the password used for the connection",
+      value: "",
+      password: true,
+    };
+    const password = await vscode.window.showInputBox(passOptions);
 
-		let contextOptions = {
-			placeHolder: "Context",
-			prompt: "Enter the cmci context used for connection",
-			value: ""
-		};
-		const context = await vscode.window.showInputBox(contextOptions);
-	
-		const session = new imperative_1.Session({
-			type: "basic",
-			hostname: hostname,
-			port: Number(port),
-			user: username,
-			password: password,
-			basePath: "",
-			rejectUnauthorized: false,
-			protocol: "http",
-		});
-		const cicsSesison = new CicsSession(session, context);
-		this.sessionMap.set(sessionName, cicsSesison);
-		await this.refresh();
-	}
+    let contextOptions = {
+      placeHolder: "Context",
+      prompt: "Enter the cmci context used for connection",
+      value: "",
+    };
+    const context = await vscode.window.showInputBox(contextOptions);
 
-	getTreeItem(element: TreeItem): vscode.TreeItem | Thenable<vscode.TreeItem> {
-		return element;
-	}
+    const session = new imperative_1.Session({
+      type: "basic",
+      hostname: hostname,
+      port: Number(port),
+      user: username,
+      password: password,
+      basePath: "",
+      rejectUnauthorized: false,
+      protocol: "http",
+    });
+    const cicsSesison = new CicsSession(session, context!);
+    this.sessionMap.set(sessionName, cicsSesison);
+    await this.refresh();
+  }
 
-	getChildren(element?: TreeItem | undefined): vscode.ProviderResult<TreeItem[]> {
-		if (element === undefined) {
-			return this.data;
-		}
-		return element.children;
-	}
+  getTreeItem(element: TreeItem): vscode.TreeItem | Thenable<vscode.TreeItem> {
+    return element;
+  }
+
+  getChildren(
+    element?: TreeItem | undefined
+  ): vscode.ProviderResult<TreeItem[]> {
+    if (element === undefined) {
+      return this.data;
+    }
+    return element.children;
+  }
 }
 
 class TreeItem extends vscode.TreeItem {
-	children: TreeItem[] | undefined;
-	session: imperative_1.Session | undefined;
+  children: TreeItem[] | undefined;
+  session: imperative_1.Session | undefined;
 
-	constructor(label: string, session: imperative_1.Session | undefined, children?: TreeItem[]) {
-		super(
-			label,
-			children === undefined ? vscode.TreeItemCollapsibleState.None :
-				vscode.TreeItemCollapsibleState.Expanded);
-		this.children = children;
-		this.session = session;
-	}
+  constructor(
+    label: string,
+    session: imperative_1.Session | undefined,
+    children?: TreeItem[]
+  ) {
+    super(
+      label,
+      children === undefined
+        ? vscode.TreeItemCollapsibleState.None
+        : vscode.TreeItemCollapsibleState.Expanded
+    );
+    this.children = children;
+    this.session = session;
+  }
 }
