afe16092811d375a9f21fd5555d02ec7c8b65fa5
AndrewTwydell <u1858490@unimail.hud.ac.uk>
2021-04-22 22:04:27 +0100

    Undo unneeded changes
    Signed-off-by: AndrewTwydell <u1858490@unimail.hud.ac.uk>

--- src/commands/addSessionCommand.ts
+++ src/commands/addSessionCommand.ts
@@ -1,15 +1,9 @@
 import { CICSTreeDataProvider } from "../trees/treeProvider";
-import { commands, window } from "vscode";
+import { commands } from "vscode";
 
 export function getAddSessionCommand(tree: CICSTreeDataProvider) {
   return commands.registerCommand(
     "cics-extension-for-zowe.addSession",
-    async () => {
-      const item = window.createStatusBarItem();
-      item.text = "Adding Session...";
-      item.show();
-      await tree.addSession();
-      item.hide();
-    }
+    async () => tree.addSession()
   );
 }
--- src/commands/refreshCommand.ts
+++ src/commands/refreshCommand.ts
@@ -1,23 +1,9 @@
 import { CICSTreeDataProvider } from "../trees/treeProvider";
-import { commands, window } from "vscode";
+import { commands } from "vscode";
 
 export function getRefreshCommand(tree: CICSTreeDataProvider) {
   return commands.registerCommand(
     "cics-extension-for-zowe.refreshTree",
-    async () => {
-      window.showInformationMessage(
-        `Refreshing Sessions`
-      );
-
-      const item = window.createStatusBarItem();
-      item.text = 'Refreshing...';
-      item.show();
-      
-      await tree.refresh();
-      window.showInformationMessage(
-        `Sessions Refresh Completed`
-      );
-      item.hide();
-    }
+    async () => tree.refresh()
   );
 }
