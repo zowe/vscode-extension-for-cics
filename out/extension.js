960fd2949160eb9b94b4724669265dc46fd7cfda
AndrewTwydell <u1858490@unimail.hud.ac.uk>
2021-06-28 12:04:22 +0100

    Adding license to each file
    Signed-off-by: AndrewTwydell <u1858490@unimail.hud.ac.uk>

--- out/extension.js
+++ out/extension.js
@@ -1,4 +1,14 @@
 "use strict";
+/*
+* This program and the accompanying materials are made available under the terms of the
+* Eclipse Public License v2.0 which accompanies this distribution, and is available at
+* https://www.eclipse.org/legal/epl-v20.html
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Copyright Contributors to the Zowe Project.
+*
+*/
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
--- src/trees/CICSProgramTree.ts
+++ src/trees/CICSProgramTree.ts
@@ -1,3 +1,14 @@
+/*
+* This program and the accompanying materials are made available under the terms of the
+* Eclipse Public License v2.0 which accompanies this distribution, and is available at
+* https://www.eclipse.org/legal/epl-v20.html
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Copyright Contributors to the Zowe Project.
+*
+*/
+
 import { TreeItemCollapsibleState, TreeItem } from "vscode";
 import { CICSRegionTreeItem } from "./CICSRegionTree";
 import { join } from "path";
@@ -14,16 +25,16 @@ export class CICSProgramTreeItem extends TreeItem {
       dark: join(__filename, "..", "..", "..", "resources", "imgs", "program-light.svg"),
     }
   ) {
+
     super(
-      `${program.program}${
-        program.status.toLowerCase() === "disabled" ? " (Disabled)" : ""
+      `${program.program}${program.status.toLowerCase() === "disabled" ? " (Disabled)" : ""
       }`,
       TreeItemCollapsibleState.None
     );
+    console.log(iconPath.light);
     this.program = program;
     this.parentRegion = parentRegion;
-    this.contextValue = `cicsprogram.${program.status.toLowerCase()}.${
-      program.program
-    }`;
+    this.contextValue = `cicsprogram.${program.status.toLowerCase()}.${program.program
+      }`;
   }
 }
