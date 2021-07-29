caeb22691f1439f9ee825614a823cb775a6d9612
AndrewTwydell <u1858490@unimail.hud.ac.uk>
2021-07-29 22:26:21 +0100

    tree item classes
    Signed-off-by: AndrewTwydell <u1858490@unimail.hud.ac.uk>

---
+++ src/trees/treeItems/CICSLocalFileTreeItem.ts
@@ -0,0 +1,39 @@
+/*
+* This program and the accompanying materials are made available under the terms of the
+* Eclipse Public License v2.0 which accompanies this distribution, and is available at
+* https://www.eclipse.org/legal/epl-v20.html
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Copyright Contributors to the Zowe Project.
+*
+*/
+
+import { TreeItemCollapsibleState, TreeItem } from "vscode";
+import { join } from "path";
+import { CICSRegionTree } from "../CICSRegionTree";
+
+export class CICSLocalFileTreeItem extends TreeItem {
+  localFile: any;
+  parentRegion: CICSRegionTree;
+  localFileName: string;
+
+  constructor(
+    localFile: any,
+    parentRegion: CICSRegionTree,
+    public readonly iconPath = {
+      light: join(__filename, "..", "..", "..", "resources", "imgs", "program-dark.svg"),
+      dark: join(__filename, "..", "..", "..", "resources", "imgs", "program-light.svg"),
+    }
+  ) {
+
+    super(
+      `${localFile.file}`,
+      TreeItemCollapsibleState.None
+    );
+    this.localFile = localFile;
+    this.contextValue = `cicslocalfile.${localFile.file}`;
+    this.parentRegion = parentRegion;
+    this.localFileName = localFile.file;
+  }
+}
---
+++ src/trees/treeItems/CICSTransactionTreeItem.ts
@@ -0,0 +1,39 @@
+/*
+* This program and the accompanying materials are made available under the terms of the
+* Eclipse Public License v2.0 which accompanies this distribution, and is available at
+* https://www.eclipse.org/legal/epl-v20.html
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Copyright Contributors to the Zowe Project.
+*
+*/
+
+import { TreeItemCollapsibleState, TreeItem } from "vscode";
+import { join } from "path";
+import { CICSRegionTree } from "../CICSRegionTree";
+
+export class CICSTransactionTreeItem extends TreeItem {
+  transaction: any;
+  parentRegion: CICSRegionTree;
+  transactionName: string;
+
+  constructor(
+    transaction: any,
+    parentRegion: CICSRegionTree,
+    public readonly iconPath = {
+      light: join(__filename, "..", "..", "..", "resources", "imgs", "program-dark.svg"),
+      dark: join(__filename, "..", "..", "..", "resources", "imgs", "program-light.svg"),
+    }
+  ) {
+
+    super(
+      `${transaction.tranid}`,
+      TreeItemCollapsibleState.None
+    );
+    this.transaction = transaction;
+    this.contextValue = `cicstransaction.TRANSACTION`;
+    this.parentRegion = parentRegion;
+    this.transactionName = transaction.tranid;
+  }
+}
