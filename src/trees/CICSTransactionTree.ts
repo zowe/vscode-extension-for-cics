ed608390139c8ab81704d1d1ee94d223a6dd46cf
AndrewTwydell <u1858490@unimail.hud.ac.uk>
2021-07-30 14:56:20 +0100

    storing filter in transaction tree
    Signed-off-by: AndrewTwydell <u1858490@unimail.hud.ac.uk>

--- src/trees/CICSTransactionTree.ts
+++ src/trees/CICSTransactionTree.ts
@@ -13,10 +13,12 @@ import { TreeItemCollapsibleState, TreeItem } from "vscode";
 import { join } from "path";
 import { CICSTransactionTreeItem } from "./treeItems/CICSTransactionTreeItem";
 import { CICSRegionTree } from "./CICSRegionTree";
+import { getResource } from "@zowe/cics-for-zowe-cli";
 
 export class CICSTransactionTree extends TreeItem {
   children: CICSTransactionTreeItem[] = [];
   parentRegion: CICSRegionTree;
+  activeFilter: string | undefined = undefined;
 
   constructor(
     parentRegion: CICSRegionTree,
@@ -42,11 +44,52 @@ export class CICSTransactionTree extends TreeItem {
     }
   ) {
     super('Transactions', TreeItemCollapsibleState.Collapsed);
-    this.contextValue = `cicstransactiontree.transactions`;
+    this.contextValue = `cicstransactiontree.${this.activeFilter ? 'filtered' : 'unfiltered'}.transactions`;
     this.parentRegion = parentRegion;
   }
 
   public addTransaction(program: CICSTransactionTreeItem) {
     this.children.push(program);
+  }
+
+  public async loadContents() {
+    try {
+      const transactionResponse = await getResource(this.parentRegion.parentSession.session, {
+        name: "CICSLocalTransaction",
+        regionName: this.parentRegion.getRegionName(),
+        cicsPlex: this.parentRegion.parentPlex ? this.parentRegion.parentPlex!.getPlexName() : undefined,
+      });
+      this.children = [];
+      for (const transaction of transactionResponse.response.records.cicslocaltransaction) {
+        if (!this.activeFilter) {
+          const newTransactionItem = new CICSTransactionTreeItem(transaction, this.parentRegion);
+          //@ts-ignore
+          this.addTransaction(newTransactionItem);
+        } else {
+          const regex = new RegExp(this.activeFilter.toUpperCase());
+          if (regex.test(transaction.tranid)) {
+            const newTransactionItem = new CICSTransactionTreeItem(transaction, this.parentRegion);
+            //@ts-ignore
+            this.addTransaction(newTransactionItem);
+          }
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  public clearFilter() {
+    this.activeFilter = undefined;
+    this.contextValue = `cicstransactiontree.${this.activeFilter ? 'filtered' : 'unfiltered'}.transactions`;
+    this.label = `Transactions`;
+    this.collapsibleState = TreeItemCollapsibleState.Expanded;
+  }
+
+  public setFilter(newFilter: string) {
+    this.activeFilter = newFilter;
+    this.contextValue = `cicstransactiontree.${this.activeFilter ? 'filtered' : 'unfiltered'}.transactions`;
+    this.label = `Transactions (${this.activeFilter})`;
+    this.collapsibleState = TreeItemCollapsibleState.Expanded;
   }
 }
