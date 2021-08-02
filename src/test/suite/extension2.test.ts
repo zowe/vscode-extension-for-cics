e1d26317b86637820182f73bc4ef4eaa6be1c867
AndrewTwydell <u1858490@unimail.hud.ac.uk>
2021-08-02 11:33:46 +0100

    added tests
    Signed-off-by: AndrewTwydell <u1858490@unimail.hud.ac.uk>

---
+++ src/test/suite/extension2.test.ts
@@ -0,0 +1,25 @@
+/*
+* This program and the accompanying materials are made available under the terms of the
+* Eclipse Public License v2.0 which accompanies this distribution, and is available at
+* https://www.eclipse.org/legal/epl-v20.html
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Copyright Contributors to the Zowe Project.
+*
+*/
+
+import * as assert from 'assert';
+
+// You can import and use all API from the 'vscode' module
+// as well as import your extension to test it
+import * as vscode from 'vscode';
+
+suite('Second Extension Test Suite', () => {
+	vscode.window.showInformationMessage('Start all tests.');
+
+	test('Another Sample test', () => {
+		assert.equal(-1, [1, 2, 3].indexOf(5));
+		assert.equal(-1, [1, 2, 3].indexOf(0));
+	});
+});
