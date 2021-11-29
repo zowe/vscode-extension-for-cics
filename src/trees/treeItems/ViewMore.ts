575811eb9673db8f7adf5cdaa4352cf11819625b
JeffinSiby <jeffinsiby@gmail.com>
2021-11-29 20:15:21 +0000

    introduce viewmore treeitem
    Signed-off-by: JeffinSiby <jeffinsiby@gmail.com>

---
+++ src/trees/treeItems/ViewMore.ts
@@ -0,0 +1,31 @@
+/*
+* This program and the accompanying materials are made available under the terms of the
+* Eclipse Public License v2.0 which accompanies this distribution, and is available at
+* https://www.eclipse.org/legal/epl-v20.html
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Copyright Contributors to the Zowe Project.
+*
+*/
+
+import { TreeItemCollapsibleState, TreeItem } from "vscode";
+
+export class ViewMore extends TreeItem {
+  parentPlex: any;
+
+  constructor(
+    parentPlex: any,
+  ) {
+    super(
+      `View x more ...`,
+      TreeItemCollapsibleState.None
+    );
+    this.parentPlex = parentPlex;
+    this.contextValue = 'viewmore.';
+    this.command = {
+      title: 'View more',
+      command: 'cics-extension-for-zowe.viewMore'
+    };
+  }
+}
