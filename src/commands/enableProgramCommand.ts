bf8d4170b5c73fe35506fb4c4b53396e83297692
JeffinSiby <jeffinsiby@gmail.com>
2021-12-03 11:54:26 +0000

    Fix functions for programs with no parent plex
    Signed-off-by: JeffinSiby <jeffinsiby@gmail.com>

--- src/commands/enableProgramCommand.ts
+++ src/commands/enableProgramCommand.ts
@@ -68,11 +68,13 @@ export function getEnableProgramCommand(tree: CICSTree, treeview: TreeView<any>)
             }
             for (const parentRegion of parentRegions) {
               const programTree = parentRegion.children!.filter((child: any) => child.contextValue.includes("cicstreeprogram."))[0];
-              const allProgramsTree = parentRegion.parentPlex!.children!.filter((child: any) => child.contextValue.includes("cicscombinedprogramtree."))[0];
               await programTree.loadContents();
-              if (allProgramsTree.collapsibleState === 2) {
-                //@ts-ignore
-                await allProgramsTree.loadContents(tree);
+              if (parentRegion.parentPlex) {
+                const allProgramsTree = parentRegion.parentPlex!.children!.filter((child: any) => child.contextValue.includes("cicscombinedprogramtree."))[0];
+                if (allProgramsTree.collapsibleState === 2) {
+                  //@ts-ignore
+                  await allProgramsTree.loadContents(tree);
+              }
               }
             }
             tree._onDidChangeTreeData.fire(undefined);
