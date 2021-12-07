8cb1ff8ddeb93fb0c86c96fac4ffa6e3c394e7e3
JeffinSiby <jeffinsiby@gmail.com>
2021-12-07 15:19:40 +0000

    Add plex filtering to Regions tree
    Signed-off-by: JeffinSiby <jeffinsiby@gmail.com>

--- src/trees/CICSRegionsContainer.ts
+++ src/trees/CICSRegionsContainer.ts
@@ -9,10 +9,12 @@
 *
 */
 
-import { TreeItemCollapsibleState, TreeItem } from "vscode";
+import { TreeItemCollapsibleState, TreeItem, window, ProgressLocation } from "vscode";
 import { join } from "path";
 import { CICSPlexTree } from "./CICSPlexTree";
 import { CICSRegionTree } from "./CICSRegionTree";
+import { CICSTree } from "./CICSTree";
+import { ProfileManagement } from "../utils/profileManagement";
 
 export class CICSRegionsContainer extends TreeItem {
   children: (CICSRegionTree)[];
@@ -49,6 +51,54 @@ export class CICSRegionsContainer extends TreeItem {
     this.children = [];
   }
 
+  public async filterRegions(pattern: string, tree: CICSTree) {
+    this.children = [];
+    const patternList = pattern.split(",");
+    let patternString = "";
+    for (const index in patternList) {
+      patternString += `(^${patternList[index].replace("*","(.*)")})`;
+      if (parseInt(index) !== patternList.length-1) {
+        patternString += "|";
+      }
+    }
+    this.activeFilter = pattern === "*" ? undefined : patternString;
+    const regex = new RegExp(patternString);
+    this.setLabel(pattern === "*" ? `Regions` : `Regions (${pattern})`);
+    window.withProgress({
+      title: 'Filtering regions',
+      location: ProgressLocation.Notification,
+      cancellable: true
+    }, async (_, token) => {
+      token.onCancellationRequested(() => {
+        console.log("Cancelling the filter");
+      });
+      const regionInfo = await ProfileManagement.getRegionInfoInPlex(this.parent);
+      let totalCount = 0;
+      let activeCount = 0;
+      for (const region of regionInfo) {
+        if (region.cicsname.match(regex)){
+          const newRegionTree = new CICSRegionTree(
+            region.cicsname, 
+            region, 
+            this.parent.parent, 
+            this.parent);
+          this.addRegion(newRegionTree);
+          totalCount += 1;
+            if (region.cicsstate === 'ACTIVE') {
+              activeCount += 1;
+            }
+        }
+      }
+      const newLabel = pattern === "*" ? `Regions [${activeCount}/${totalCount}]` : `Regions (${pattern}) [${activeCount}/${totalCount}]`;
+      this.setLabel(newLabel);
+      tree._onDidChangeTreeData.fire(undefined);
+      if (!this.children.length){
+        window.showInformationMessage(`No regions found for ${this.parent.getPlexName()}`);
+      }
+    });
+  
+  }
+
   public addRegion(region: CICSRegionTree) {
     this.children.push(region);
   }
@@ -59,5 +109,9 @@ export class CICSRegionsContainer extends TreeItem {
 
   public setLabel(label: string) {
     this.label = label;
+  }
+
+  public getParent() {
+    return this.parent;
   }
 }
