c27d08d745988af7982a88cc3805b72457b24a0b
JeffinSiby <jeffinsiby@gmail.com>
2021-12-13 12:00:47 +0000

    Update plex icon
    Signed-off-by: JeffinSiby <jeffinsiby@gmail.com>

--- src/trees/CICSPlexTree.ts
+++ src/trees/CICSPlexTree.ts
@@ -33,7 +33,7 @@ export class CICSPlexTree extends TreeItem {
     plexName: string,
     profile: IProfileLoaded,
     sessionTree: CICSSessionTree,
-    public readonly iconPath = getIconPathInResources("list-dark.svg", "list-light.svg")
+    public readonly iconPath = getIconPathInResources("cics-plex-dark.svg", "cics-plex-light.svg")
   ) {
     super(plexName, TreeItemCollapsibleState.Collapsed);
     this.plexName = plexName;
@@ -63,38 +63,44 @@ export class CICSPlexTree extends TreeItem {
     this.addRegion(newRegionTree);
   }
 
-  // Store all filters on children resources
-  public findResourceFilters() {
-    for (const region of this.children) {
-      if (region instanceof CICSRegionTree) {
-        if (region.children) {
-          for (const resourceTree of region.children) {
-            const filter = resourceTree.getFilter();
-            if (filter) {
-              this.resourceFilters[region.getRegionName()] = {[resourceTree.label!.toString().split(' ')[0]]: filter};
-            }
-          }
-        }
-      }
-    }
-  }
+  // // Store all filters on children resources
+  // public findResourceFilters() {
+  //   const regionsContainer = this.children.filter(child => child instanceof CICSRegionsContainer)[0];
+  //   if (regionsContainer){
+  //     for (const region of regionsContainer.getChildren()!) {
+  //       if (region instanceof CICSRegionTree) {
+  //         if (region.children) {
+  //           for (const resourceTree of region.children) {
+  //             const filter = resourceTree.getFilter();
+  //             if (filter) {
+  //               this.resourceFilters[region.getRegionName()] = {[resourceTree.label!.toString().split(' ')[0]]: filter};
+  //             } else {
+  //               this.resourceFilters[region.getRegionName()] = {[resourceTree.label!.toString().split(' ')[0]]: undefined};
+  //             }
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
 
-  public async reapplyFilter() {/////////////////
-    for (const region of this.children) {
-      if (region instanceof CICSRegionTree) {
-        const resourceFilters = this.getResourceFilter(region.getRegionName());
-        if (resourceFilters) {
-          for (const resourceTree of region.children!) {
-            if (resourceFilters[resourceTree.label!.toString().split(' ')[0]]) {
-              resourceTree.setFilter(resourceFilters[resourceTree.label!.toString()]);
-              await resourceTree.loadContents();
-              resourceTree.collapsibleState = TreeItemCollapsibleState.Expanded;
-            }
-          }
-        }
-      }
-    }
-  }
+  // public async reapplyFilter() {
+  //   const regionsContainer = this.children.filter(child => child instanceof CICSRegionsContainer)[0];
+  //   for (const region of regionsContainer.getChildren()!) {
+  //     if (region instanceof CICSRegionTree) {
+  //       const resourceFilters = this.getResourceFilter(region.getRegionName());
+  //       if (resourceFilters) {
+  //         for (const resourceTree of region.children!) {
+  //           if (resourceFilters[resourceTree.label!.toString().split(' ')[0]]) {
+  //             resourceTree.setFilter(resourceFilters[resourceTree.label!.toString()]);
+  //             await resourceTree.loadContents();
+  //             resourceTree.collapsibleState = TreeItemCollapsibleState.Expanded;
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
 
   public getResourceFilter(regionName: string) {
     return this.resourceFilters[regionName];
