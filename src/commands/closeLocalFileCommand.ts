f8483daedb38739082192d59ae273664c7201a89
JeffinSiby <jeffinsiby@gmail.com>
2022-01-25 10:20:54 +0000

    fix cmd palette action for close local file
    Signed-off-by: JeffinSiby <jeffinsiby@gmail.com>

--- src/commands/closeLocalFileCommand.ts
+++ src/commands/closeLocalFileCommand.ts
@@ -20,101 +20,106 @@ import { CICSRegionTree } from "../trees/CICSRegionTree";
 import { CICSTree } from "../trees/CICSTree";
 import * as https from "https";
 import { CICSRegionsContainer } from "../trees/CICSRegionsContainer";
+import { CICSLocalFileTreeItem } from "../trees/treeItems/CICSLocalFileTreeItem";
+import { findSelectedNodes } from "../utils/commandUtils";
 
 export function getCloseLocalFileCommand(tree: CICSTree, treeview: TreeView<any>) {
   return commands.registerCommand(
     "cics-extension-for-zowe.closeLocalFile",
     async (clickedNode) => {
-      if (clickedNode) {
-        try {
-          let busyDecision = await window.showInformationMessage(
-            `Choose one of the following for the file busy condition`,
-            ...["Wait", "No Wait", "Force"]);
-          if (busyDecision){
-            busyDecision =  busyDecision.replace(" ","").toUpperCase();
-            const selectedNodes = treeview.selection.filter((selectedNode) => selectedNode !== clickedNode);
-            const allSelectedNodes = [clickedNode, ...selectedNodes];
-            let parentRegions: CICSRegionTree[] = [];
-
-            window.withProgress({
-              title: 'Close',
-              location: ProgressLocation.Notification,
-              cancellable: true
-            }, async (progress, token) => {
-              token.onCancellationRequested(() => {
-                console.log("Cancelling the Close");
-              });
-              for (const index in allSelectedNodes) {
-                progress.report({
-                  message: `Closing ${parseInt(index) + 1} of ${allSelectedNodes.length}`,
-                  increment: (parseInt(index) / allSelectedNodes.length) * 100,
-                });
-                try {
-                  const currentNode = allSelectedNodes[parseInt(index)];
-
-                  https.globalAgent.options.rejectUnauthorized = currentNode.parentRegion.parentSession.session.ISession.rejectUnauthorized;
-
-                  await closeLocalFile(
-                    currentNode.parentRegion.parentSession.session,
-                    {
-                      name: currentNode.localFile.file,
-                      regionName: currentNode.parentRegion.label,
-                      cicsPlex: currentNode.parentRegion.parentPlex ? currentNode.parentRegion.parentPlex.getPlexName() : undefined,
-                    },
-                    busyDecision!
-                  );
-                  https.globalAgent.options.rejectUnauthorized = undefined;
-                  if (!parentRegions.includes(currentNode.parentRegion)) {
-                    parentRegions.push(currentNode.parentRegion);
-                  }
-                } catch (err) {
-                  https.globalAgent.options.rejectUnauthorized = undefined;
-                  // @ts-ignore
-                  const mMessageArr = err.mMessage.split(" ").join("").split("\n");
-                  let resp;
-                  let resp2;
-                  let respAlt;
-                  let eibfnAlt;
-                  for (const val of mMessageArr) {
-                    const values = val.split(":");
-                    if (values[0] === "resp"){
-                      resp = values[1];
-                    } else if (values[0] === "resp2"){
-                      resp2 = values[1];
-                    } else if (values[0] === "resp_alt"){
-                      respAlt = values[1];
-                    } else if (values[0] === "eibfn_alt"){
-                      eibfnAlt = values[1];
-                    }
-                  }
-                  window.showErrorMessage(`Perform CLOSE on Local file "${allSelectedNodes[parseInt(index)].localFile.file}" failed: EXEC CICS command (${eibfnAlt}) RESP(${respAlt}) RESP2(${resp2})`);
-                }
-              }
-              for (const parentRegion of parentRegions) {
-                const localFileTree = parentRegion.children!.filter((child: any) => child.contextValue.includes("cicstreelocalfile."))[0];
-                // Only load contents if the tree is expanded
-                if (localFileTree.collapsibleState === 2) {
-                  await localFileTree.loadContents();
-                }
-                // if node is in a plex and the plex contains the region container tree
-                if (parentRegion.parentPlex && parentRegion.parentPlex.children.some((child) => child instanceof CICSRegionsContainer)) {
-                  const allLocalFileTreeTree = parentRegion.parentPlex.children!.filter((child: any) => child.contextValue.includes("cicscombinedlocalfiletree."))[0];
-                  //@ts-ignore
-                  if (allLocalFileTreeTree.collapsibleState === 2 && allLocalFileTreeTree.getActiveFilter()) {
-                    //@ts-ignore
-                    await allLocalFileTreeTree.loadContents(tree);
-                  }
-                }
-              }
-              tree._onDidChangeTreeData.fire(undefined);
-            });
-          }
-        } catch (err) {
-          // @ts-ignore
-          window.showErrorMessage(err);
-        }
-      } else {
+      const allSelectedNodes = findSelectedNodes(treeview, CICSLocalFileTreeItem, clickedNode);
+      if (!allSelectedNodes || !allSelectedNodes.length) {
         window.showErrorMessage("No CICS local file selected");
+        return;
+      }
+      let parentRegions: CICSRegionTree[] = [];
+      let busyDecision = await window.showInformationMessage(
+        `Choose one of the following for the file busy condition`,
+        ...["Wait", "No Wait", "Force"]);
+      if (busyDecision){
+        busyDecision =  busyDecision.replace(" ","").toUpperCase();
+
+        window.withProgress({
+          title: 'Close',
+          location: ProgressLocation.Notification,
+          cancellable: true
+        }, async (progress, token) => {
+          token.onCancellationRequested(() => {
+            console.log("Cancelling the Close");
+          });
+          for (const index in allSelectedNodes) {
+            progress.report({
+              message: `Closing ${parseInt(index) + 1} of ${allSelectedNodes.length}`,
+              increment: (parseInt(index) / allSelectedNodes.length) * 100,
+            });
+            const currentNode = allSelectedNodes[parseInt(index)];
+            
+            https.globalAgent.options.rejectUnauthorized = currentNode.parentRegion.parentSession.session.ISession.rejectUnauthorized;
+            
+            try {
+              await closeLocalFile(
+                currentNode.parentRegion.parentSession.session,
+                {
+                  name: currentNode.localFile.file,
+                  regionName: currentNode.parentRegion.label,
+                  cicsPlex: currentNode.parentRegion.parentPlex ? currentNode.parentRegion.parentPlex.getPlexName() : undefined,
+                },
+                busyDecision!
+              );
+              https.globalAgent.options.rejectUnauthorized = undefined;
+              if (!parentRegions.includes(currentNode.parentRegion)) {
+                parentRegions.push(currentNode.parentRegion);
+              }
+            } catch (error) {
+              https.globalAgent.options.rejectUnauthorized = undefined;
+              // @ts-ignore
+              if (error.mMessage) {
+                // @ts-ignore
+                const mMessageArr = error.mMessage.split(" ").join("").split("\n");
+                let resp;
+                let resp2;
+                let respAlt;
+                let eibfnAlt;
+                for (const val of mMessageArr) {
+                  const values = val.split(":");
+                  if (values[0] === "resp"){
+                    resp = values[1];
+                  } else if (values[0] === "resp2"){
+                    resp2 = values[1];
+                  } else if (values[0] === "resp_alt"){
+                    respAlt = values[1];
+                  } else if (values[0] === "eibfn_alt"){
+                    eibfnAlt = values[1];
+                  }
+                }
+                window.showErrorMessage(`Perform CLOSE on local file "${allSelectedNodes[parseInt(index)].localFile.file}" failed: EXEC CICS command (${eibfnAlt}) RESP(${respAlt}) RESP2(${resp2})`);
+              } else {
+                window.showErrorMessage(`Something went wrong when performing a CLOSE - ${JSON.stringify(error, Object.getOwnPropertyNames(error)).replace(/(\\n\t|\\n|\\t)/gm," ")}`);
+              }
+            }
+          }
+          for (const parentRegion of parentRegions) {
+            try {
+              const localFileTree = parentRegion.children!.filter((child: any) => child.contextValue.includes("cicstreelocalfile."))[0];
+              // Only load contents if the tree is expanded
+              if (localFileTree.collapsibleState === 2) {
+                await localFileTree.loadContents();
+              }
+              // if node is in a plex and the plex contains the region container tree
+              if (parentRegion.parentPlex && parentRegion.parentPlex.children.some((child) => child instanceof CICSRegionsContainer)) {
+                const allLocalFileTreeTree = parentRegion.parentPlex.children!.filter((child: any) => child.contextValue.includes("cicscombinedlocalfiletree."))[0];
+                //@ts-ignore
+                if (allLocalFileTreeTree.collapsibleState === 2 && allLocalFileTreeTree.getActiveFilter()) {
+                  //@ts-ignore
+                  await allLocalFileTreeTree.loadContents(tree);
+                }
+              }
+            } catch(error) {
+              window.showErrorMessage(`Something went wrong when reloading local files - ${JSON.stringify(error, Object.getOwnPropertyNames(error)).replace(/(\\n\t|\\n|\\t)/gm," ")}`);
+            }
+          }
+          tree._onDidChangeTreeData.fire(undefined);
+        });
       }
     }
   );
